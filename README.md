aa6e0f415ef0ab829af32efeb4140af33820345e
Harrison <harrison.campbell0101@gmail.com>
2023-05-29 13:51:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# Video Frame Metadata Extraction
+
+This Python script is designed to extract metadata from an SRT file and write it to the frames of a video. It utilizes the `ffmpeg` and `exiftool` command-line tools to split the video into frames and write the metadata, respectively.
+
+## Prerequisites
+
+-   Python 3.x
+-   `ffmpeg` command-line tool
+-   `exiftool` command-line tool
+
+## Usage
+
+1. Ensure that you have Python 3.x installed on your system.
+2. Install the required Python dependencies by running the following command:
+
+    ```shell
+    pip install argparse
+    ```
+
+3. Install `ffmpeg` and `exiftool` on your system. Instructions for installation can be found on the official websites of the respective tools.
+4. Download or clone the script to your local machine.
+
+5. Open a terminal or command prompt and navigate to the directory containing the script.
+
+6. Run the following command to execute the script:
+
+    ```shell
+    python script_name.py --video <path_to_video> --srt <path_to_srt> [--framerate <framerate>] [--framerate-video <framerate_video>] [--frames_dir <frames_dir>] [--metadata-file <metadata_file>]
+    ```
+
+    Replace `script_name.py` with the actual name of the script file.
+
+    #### Parameters
+
+    - `--video`: (required) Path to the video file.
+    - `--srt`: (required) Path to the SRT file containing metadata.
+    - `--framerate`: (optional) Framerate at which to output the frames. Default is `1`.
+    - `--framerate-video`: (optional) Framerate of the input video. Default is `30`.
+    - `--frames_dir`: (optional) Path to the directory where the frames will be saved. Default is `'frames/'`.
+    - `--metadata-file`: (optional) Path to the metadata image file. Default is `'metadata.jpg'`.
+
+    **Note**: Make sure to replace `<path_to_video>`, `<path_to_srt>`, and other parameters with the actual paths and values.
+
+7. The script will perform the following actions:
+
+    - Parse the SRT file to extract frame numbers, timestamps, and metadata.
+    - Split the video into frames using `ffmpeg`.
+    - Write the metadata from the metadata image file to the frames using `exiftool`.
+    - Write the extracted metadata to the frames using `exiftool`.
+
+8. After the script finishes executing, you will find the frames with the embedded metadata in the specified `--frames_dir` directory.
+
+## Example
+
+Here is an example command to run the script:
+
+```shell
+python video_frame_metadata_extraction.py --video input_video.mp4 --srt input_metadata.srt --framerate 2 --framerate-video 30 --frames_dir output_frames --metadata-file metadata.jpg
+```
+
+In this example, the script will extract metadata from the `input_metadata.srt` file and embed it into the frames of the `input_video.mp4` video. The frames will be saved in the `output_frames` directory, and the metadata will be written using the `metadata.jpg` file.
+
+The `metadata.jpg` file should be a photo you have taken with the device that captured the video.
+
+Feel free to adjust the parameters according to your specific requirements.
---
+++ vid_to_gpsFrames.py
@@ -0,0 +1,99 @@
+import os
+from argparse import ArgumentParser
+from datetime import datetime
+
+def parse_srt_file(path, framerate, framerate_video):
+    with open(path, 'r') as f:
+        frame_data = []
+
+        # The SRT file will be formatted as follows
+
+        # 1
+        # 00:00:00,000 --> 00:00:00,033
+        # <font size="28">SrtCnt : 1, DiffTime : 33ms
+        # 2023-05-02 19:40:59.725
+        # [iso : 100] [shutter : 1/125.0] [fnum : 170] [ev : 0] [ct : 5289] [color_md : default] [focal_len : 240] [dzoom_ratio: 10000, delta:0],[latitude: 53.578046] [longitude: -1.775009] [rel_alt: 34.600 abs_alt: 137.305] </font>
+        #
+        # 2
+        # etc...
+
+        # We want to extract the frame number, the timestamp, and the data in the square brackets
+        # We will store this in a list of dictionaries, where each dictionary represents a frame
+
+        lines = f.readlines()
+        for i in range(0, len(lines), int(6 * (framerate_video / framerate))):
+            frame = {}
+            frame['frame'] = int(lines[i].strip())
+            frame['timestamp'] = lines[i+3].strip()
+
+            # The data is in the square brackets, so we split the line by ']' and take the second half
+            # We then split this by ' : ' and take the second half
+            # This gives us the data we want
+            attributes = lines[i+4].split(']')
+
+            for attr in attributes:
+                if ': ' in attr:
+                    frame[attr.split(": ")[0].replace('[', '').strip()] = attr.split(": ")[1]
+
+            frame_data.append(frame)
+        return frame_data
+
+def split_video_into_frames(path_to_video, path_to_frames, framerate):
+    # Check if the frames directory exists
+    if not os.path.exists(path_to_frames):
+        # We first create the directory to store the frames
+        os.mkdir(path_to_frames)
+
+        # The command to split the video into frames
+        # We use ffmpeg to do this
+        # The command will be formatted as follows
+        # ffmpeg -i <path_to_video> -r <framerate> <path_to_frames>/frame%04d.jpg
+        command = f"ffmpeg -hwaccel cuda -i {path_to_video} -r {framerate} {path_to_frames}/%04d.jpg"
+
+        # We run the command
+        os.system(command)
+
+def write_data_to_frame(path_to_frames, frame_data):
+    # Get files in the frames directory
+    files = os.listdir(path_to_frames)
+
+    for index in range(len(files)):
+        file = files[index]
+
+        # Data associated with frame
+        data = None
+        if index < len(frame_data):
+            data = frame_data[index]
+        else:
+            data = frame_data[-1]
+
+        # The command to write the data to the frame
+        # We use exiftool to do this
+        ts = datetime.strptime(data['timestamp'], "%Y-%m-%d %H:%M:%S.%f")
+        cmd = f"exiftool {path_to_frames}/{index+1:04d}.jpg -overwrite_original -GPSLatitude={float(data[',latitude'])} -GPSLongitude={float(data['longitude'])} -GPSAltitude={float(data['rel_alt'].split(' ')[0])} -GPSLatitudeRef=N -GPSLongitudeRef=W -GPSAltitudeRef=0 -DateTimeOriginal={ts.strftime('%Y-%m-%d %H:%M:%S.%f')} -ISO = {data['iso']} -ShutterSpeedValue={data['shutter']} -FNumber={data['fnum']} -ExposureCompensation={data['ev']} -ColorTemperature={data['ct']} -FocalLength={data['focal_len']}"
+        os.system(cmd)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('--video', type=str, required=True, help='Path to video')
+    parser.add_argument('--srt', type=str, required=True, help='Path to SRT file')
+    parser.add_argument('--framerate', type=int, default=1, help='Framerate to output frames at')
+    parser.add_argument('--framerate-video', type=int, default=30, help='Framerate of the input video')
+    parser.add_argument('--frames_dir', type=str, default='frames/', help='Path to output frames to')
+    parser.add_argument('--metadata-file', type=str, default='metadata.jpg', help='Path to metadata image')
+
+    args = parser.parse_args()
+
+    # Parse the SRT file
+    frame_data = parse_srt_file(args.srt, args.framerate, args.framerate_video)
+
+    # Split the video into frames
+    split_video_into_frames(args.video, args.frames_dir, args.framerate)
+
+    # Write all metadata from metadata file to the frames
+    os.system(f"exiftool -overwrite_original -TagsFromFile {args.metadata_file} -all:all {args.frames_dir}")
+
+    # Write the data to the frames
+    write_data_to_frame(args.frames_dir, frame_data)
+
